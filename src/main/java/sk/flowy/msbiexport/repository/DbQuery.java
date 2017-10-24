f1f18611cd053034635c82e2be5b5a9e696cd7e9
m.jarembinsky <marian.jarembinsky@codexa.sk>
2017-10-24 14:08:48 +0200

    fixing: Duplicates in exported files.

--- src/main/java/sk/flowy/msbiexport/repository/DbQuery.java
+++ src/main/java/sk/flowy/msbiexport/repository/DbQuery.java
@@ -14,46 +14,55 @@ public class DbQuery {
         switch(tableName) {
             case "klient" : return "SELECT * FROM " + tableName + " WHERE id = " + klientID;
             case "dodavatel_klient" : return "SELECT * FROM " + tableName + " WHERE id_klient = " + klientID;
-            case "dodavatel" : return "SELECT dodavatel.* FROM dodavatel INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
-            case "dodavatel_produkt" : return "SELECT dodavatel_produkt.* FROM dodavatel_produkt INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
+            case "dodavatel" : return "SELECT dodavatel.* FROM dodavatel " +
                     "INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
-            case "produkt" : return "SELECT produkt.* FROM produkt INNER JOIN dodavatel_produkt ON produkt.id = dodavatel_produkt.id_produkt INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
+            case "dodavatel_produkt" : return "SELECT DISTINCT dodavatel_produkt.* FROM dodavatel_produkt " +
+                    "INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
                     "INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
-            case "ean" : return "SELECT ean.* FROM ean INNER JOIN produkt ON ean.id_produkt = produkt.id " +
-            " INNER JOIN dodavatel_produkt ON produkt.id = dodavatel_produkt.id_produkt INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id" +
-            " INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
+            case "produkt" : return "SELECT DISTINCT produkt.* FROM produkt " +
+                    "INNER JOIN dodavatel_produkt ON produkt.id = dodavatel_produkt.id_produkt " +
+                    "INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
+                    "INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
+            case "ean" : return "SELECT DISTINCT ean.* FROM ean INNER JOIN produkt ON ean.id_produkt = produkt.id " +
+                    "INNER JOIN dodavatel_produkt ON produkt.id = dodavatel_produkt.id_produkt " +
+                    "INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
+                    "INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
             case "cenotvorba" : return "SELECT cenotvorba.* FROM cenotvorba WHERE cenotvorba.id_klient = " + klientID;
-            case "tags" : return "SELECT tags.* FROM tags INNER JOIN cenotvorba ON tags.id = cenotvorba.id_tag AND cenotvorba.id_klient = " + klientID;
+            case "tags" : return "SELECT tags.* FROM tags " +
+                    "INNER JOIN cenotvorba ON tags.id = cenotvorba.id_tag AND cenotvorba.id_klient = " + klientID;
             case "tag_produkt" : return "SELECT tag_produkt.* FROM tag_produkt INNER JOIN tags ON tags.id = tag_produkt.id_tag " +
                     "INNER JOIN cenotvorba ON tags.id = cenotvorba.id_tag AND cenotvorba.id_klient = " + klientID;
             case "uzivatel" : return "SELECT uzivatel.* FROM uzivatel WHERE uzivatel.id_klient = " + klientID;
-            case "log_prihlasenie" : return "SELECT log_prihlasenie.* FROM log_prihlasenie INNER JOIN uzivatel ON log_prihlasenie.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
-            case "prijemka" : return "SELECT prijemka.* FROM prijemka INNER JOIN uzivatel ON uzivatel.id = prijemka.id_uzivatel AND uzivatel.id_klient = " + klientID;
+            case "log_prihlasenie" : return "SELECT log_prihlasenie.* FROM log_prihlasenie " +
+                    "INNER JOIN uzivatel ON log_prihlasenie.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
+            case "prijemka" : return "SELECT prijemka.* FROM prijemka " +
+                    "INNER JOIN uzivatel ON uzivatel.id = prijemka.id_uzivatel AND uzivatel.id_klient = " + klientID;
             //no real reference from table uzivatel, only some column id_uzivatel in blocek table.
-            case "blocek" : return "SELECT blocek.* FROM blocek INNER JOIN uzivatel ON blocek.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
+            case "blocek" : return "SELECT blocek.* FROM blocek " +
+                    "INNER JOIN uzivatel ON blocek.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
             // in some logs is possible to get only through uzivatel.
             case "log_cart_bill" : return"SELECT log_cart_bill.* FROM log_cart_bill INNER JOIN  blocek ON log_cart_bill.id_bill = blocek.id " +
                     "INNER JOIN uzivatel ON blocek.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
-            case "log_cart_storno" : return " SELECT log_cart_storno.* FROM log_cart_storno " +
+            case "log_cart_storno" : return " SELECT DISTINCT log_cart_storno.* FROM log_cart_storno " +
                     "INNER JOIN  blocek ON log_cart_storno.id_cart_bill = blocek.id " +
                     "INNER JOIN uzivatel ON blocek.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
-            case "log_cart_item" : return " SELECT log_cart_item.* FROM log_cart_item " +
+            case "log_cart_item" : return " SELECT DISTINCT log_cart_item.* FROM log_cart_item " +
                     "INNER JOIN  blocek ON log_cart_item.id_cart_bill = blocek.id " +
                     "INNER JOIN uzivatel ON blocek.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
-            case "log_cart_item_storno" : return " SELECT log_cart_item_storno.* FROM log_cart_item_storno " +
+            case "log_cart_item_storno" : return " SELECT DISTINCT log_cart_item_storno.* FROM log_cart_item_storno " +
                     "INNER JOIN  blocek ON log_cart_item_storno.id_cart_bill = blocek.id " +
                     "INNER JOIN uzivatel ON blocek.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
             case "blocek_polozky" : return "SELECT blocek_polozky.* FROM blocek_polozky INNER JOIN  blocek ON blocek_polozky.id_blocek = blocek.id " +
                     "INNER JOIN uzivatel ON blocek.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
-            case "obrazky" : return "SELECT obrazky.* FROM obrazky INNER JOIN produkt ON obrazky.product_id = produkt.id " +
+            case "obrazky" : return "SELECT DISTINCT obrazky.* FROM obrazky INNER JOIN produkt ON obrazky.product_id = produkt.id " +
                     "INNER JOIN dodavatel_produkt ON produkt.id = dodavatel_produkt.id_produkt " +
                     "INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
                     "INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
-            case "stavy" : return "SELECT stavy.* FROM stavy INNER JOIN produkt ON stavy.id_produkt = produkt.id " +
+            case "stavy" : return "SELECT DISTINCT stavy.* FROM stavy INNER JOIN produkt ON stavy.id_produkt = produkt.id " +
                     "INNER JOIN dodavatel_produkt ON produkt.id = dodavatel_produkt.id_produkt " +
                     "INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
                     "INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
-            case "druh" : return "SELECT druh.* FROM druh INNER JOIN produkt ON druh.id = produkt.nonBlockable " +
+            case "druh" : return "SELECT DISTINCT druh.* FROM druh INNER JOIN produkt ON druh.id = produkt.nonBlockable " +
                     "INNER JOIN dodavatel_produkt ON produkt.id = dodavatel_produkt.id_produkt " +
                     "INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
                     "INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
@@ -63,7 +72,7 @@ public class DbQuery {
                     "INNER JOIN objednavky ON objednavky_produkt.objednavka_id = objednavky.id AND objednavky.id_klient = " + klientID;
             case "inventura" : return "SELECT inventura.* FROM inventura " +
                     "INNER JOIN uzivatel ON inventura.id_uzivatel = uzivatel.id AND uzivatel.id_klient = " + klientID;
-            case "atributy_produkt" : return "SELECT atributy_produkt.* FROM atributy_produkt INNER JOIN produkt ON atributy_produkt.id_produkt = produkt.id " +
+            case "atributy_produkt" : return "SELECT DISTINCT atributy_produkt.* FROM atributy_produkt INNER JOIN produkt ON atributy_produkt.id_produkt = produkt.id " +
                     "INNER JOIN dodavatel_produkt ON produkt.id = dodavatel_produkt.id_produkt " +
                     "INNER JOIN dodavatel ON dodavatel_produkt.id_dodavatel = dodavatel.id " +
                     "INNER JOIN dodavatel_klient ON dodavatel.id = dodavatel_klient.id_dodavatel AND dodavatel_klient.id_klient = " + klientID;
